6482ba2cdaf7b7c01a3432538f5c409350838ca8
Maisam671 <141740027+Maisam671@users.noreply.github.com>
2025-10-27 15:24:44 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -147,5 +147,5 @@ async def predict(
         return JSONResponse({"success": False, "error": str(e)})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # default to 5000
+    port = int(os.environ.get("PORT", 5000))  # default to 5000
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
